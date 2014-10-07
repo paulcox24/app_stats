e09006b5233cae9fd7808ee1078b5fe8f5bb5e33
Paul Cox <paulcox24@gmail.com>
2014-10-07 14:09:07 -0600

    moved out helpers

--- lib/app_stats/stats.rb
+++ lib/app_stats/stats.rb
@@ -16,27 +16,6 @@ module AppStats
     def self.get_has_many_relationships
       get_raw(HAS_MANY_SEARCH).split("\n")
     end
-
-    def self.get_has_many_assocations(options={})
-      ul(AppStats::Stats.get_has_many_relationships, options)
-    end
-
-    # http://qerub.se/list-helper-for-rails
-    def self.html_list(type, elements, options = {})
-      if elements.empty?
-        "" 
-      else
-        lis = elements.map{ |x| content_tag("li", x) }
-        content_tag(type, lis.join.html_safe, options)
-      end
-    end
-
-    def self.ul(*args)
-      html_list("ul", *args)
-    end
-
-    def self.ol(*args)
-      html_list("ol", *args)
-    end
+    
   end
 end
